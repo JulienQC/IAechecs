0534b580b0e54f5b09cee591c2afb5660f231311
TLOBen <32582328+TLOBen@users.noreply.github.com>
2017-11-06 14:10:55 -0500

    Added bat file

---
+++ chess.bat
@@ -0,0 +1,5 @@
+START C:\Users\Benjamin\Documents\Divers\UQAC\workspace\"8INF846 - Intelligence Artificielle"\"Plateforme de base"\jeu_echec_stage\jeu_echec_stage\bin\Release\jeu_echec_stage.exe
+timeout 3
+START C:\Users\Benjamin\Documents\Divers\UQAC\workspace\"8INF846 - Intelligence Artificielle"\"Plateforme de base"\processAI1\processAI1\bin\Release\processAI1.exe
+timeout 3
+START C:\Users\Benjamin\Documents\Divers\UQAC\workspace\"8INF846 - Intelligence Artificielle"\"Plateforme de base"\processAI2\processAI2\bin\Release\processAI2.exe
